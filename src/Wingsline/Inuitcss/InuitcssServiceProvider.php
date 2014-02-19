38e0dc50861dc010ffca08d4396b1775f25c49c2
Arpad Olasz <arpi@wingsline.com>
2014-02-18 18:09:38 -0600

    Updating to 4.1

--- src/Wingsline/Inuitcss/InuitcssServiceProvider.php
+++ src/Wingsline/Inuitcss/InuitcssServiceProvider.php
@@ -2,48 +2,48 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class InuitcssServiceProvider extends ServiceProvider {
+class InuitcssServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('wingsline/inuitcss');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('wingsline/inuitcss');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['inuitcssform'] = $this->app->share(function($app)
-		{
-			$form = new Html\FormBuilder($app['html'], $app['url'], $app['session']->getToken());
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['inuitcssform'] = $this->app->share(function ($app) {
+            $form = new Html\FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
 
-			return $form->setSessionStore($app['session']);
-		});
-	}
+            return $form->setSessionStore($app['session.store']);
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
